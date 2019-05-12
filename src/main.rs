10b99cf6adddd185ec74a609b9d10c3f8a0f1592
Odin Dutton <odindutton@gmail.com>
2019-05-12 20:05:33 +0800

    Initial commit
    Broken Game of Life. I'm committing this because I kind of like it.

---
+++ src/main.rs
@@ -0,0 +1,198 @@
+//! Game of Life
+//!
+//! Rules:
+//!
+//!   Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//!   Any live cell with two or three live neighbours lives on to the next generation.
+//!   Any live cell with more than three live neighbours dies, as if by overpopulation.
+//!   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+//!
+//! Glider:
+//!
+//!      #
+//!   #  #
+//!    # #
+
+#[derive(Clone)]
+struct Cell {
+    alive: bool,
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Self { alive: false }
+    }
+}
+
+impl std::fmt::Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        if self.alive {
+            write!(f, "# ")?;
+        } else {
+            write!(f, "  ")?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct World {
+    cells: Vec<Vec<Cell>>,
+}
+
+impl World {
+    fn birth_cell(&mut self, x: usize, y: usize) {
+        self.cells[y][x].alive = true
+    }
+
+    fn kill_cell(&mut self, x: usize, y: usize) {
+        self.cells[y][x].alive = false
+    }
+
+    fn is_cell_alive(&self, x: u8, y: u8) -> bool {
+        self.cells[y as usize][x as usize].alive
+    }
+
+    fn live_neighbours_count(&self, x: u8, y: u8) -> u8 {
+        let x = x as isize;
+        let y = y as isize;
+        let mut n = 0;
+
+        #[rustfmt::skip]
+        let offsets = vec![
+            (-1, -1), (-1, 0), (-1, 1),
+            ( 0, -1),          ( 0, 1),
+            ( 1, -1), ( 1, 0), ( 1, 1),
+        ];
+
+        for (x_offset, y_offset) in offsets {
+            let xx = x + x_offset;
+            let yy = y + y_offset;
+
+            if xx >= 0 && yy >= 0 && xx < WIDTH as isize && yy < HEIGHT as isize {
+                if self.is_cell_alive(xx as u8, yy as u8) {
+                    n += 1
+                }
+            }
+        }
+
+        n
+    }
+
+    fn simulate(&mut self) {
+        let old_world = self.clone();
+
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                let live_neighbours_count = old_world.live_neighbours_count(x, y);
+
+                if old_world.is_cell_alive(x, y) {
+                    if live_neighbours_count < 2 {
+                        // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                        self.kill_cell(x as usize, y as usize);
+                    } else if live_neighbours_count == 2 || live_neighbours_count == 3 {
+                        // Any live cell with two or three live neighbours lives on to the next generation.
+                    } else if live_neighbours_count > 3 {
+                        // Any live cell with more than three live neighbours dies, as if by overpopulation.
+                        self.kill_cell(x as usize, y as usize);
+                    }
+                } else {
+                    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                    if live_neighbours_count == 2 {
+                        self.birth_cell(x as usize, y as usize);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Default for World {
+    fn default() -> Self {
+        let mut cells = vec![];
+
+        for _ in 0..HEIGHT {
+            let mut row = vec![];
+            for _ in 0..WIDTH {
+                row.push(Cell::default())
+            }
+            cells.push(row);
+        }
+
+        Self { cells }
+    }
+}
+
+const HEIGHT: u8 = 40;
+const WIDTH: u8 = 80;
+
+fn main() {
+    let mut world = World::default();
+
+    world.birth_cell(2, 0);
+    world.birth_cell(2, 1);
+    world.birth_cell(2, 2);
+    world.birth_cell(0, 1);
+    world.birth_cell(1, 2);
+
+    let mut i = 0;
+    loop {
+        clear_screen();
+        draw_world(&world);
+        draw_info(i);
+
+        world.simulate();
+
+        i += 1;
+
+        use std::{thread, time};
+        let d = time::Duration::from_millis(250);
+        thread::sleep(d);
+    }
+}
+
+fn clear_screen() {
+    print!("\x1b[2J\x1b[1;1H");
+}
+
+fn draw_world(world: &World) {
+    for row in &world.cells {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
+
+fn draw_info(i: usize) {
+    println!();
+    println!("Iterations: {}", i);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        // 1 2 3
+        // 8 x 4
+        // 7 6 5
+        let cells = vec![
+            vec![Cell::default(), Cell::default(), Cell::default()],
+            vec![Cell::default(), Cell::default(), Cell::default()],
+            vec![Cell::default(), Cell::default(), Cell::default()],
+        ];
+        let mut world = World { cells };
+        world.birth_cell(0, 0);
+
+        assert_eq!(world.live_neighbours_count(1, 1), 1);
+
+        world.birth_cell(1, 0);
+        assert_eq!(world.live_neighbours_count(1, 1), 2);
+
+        world.birth_cell(0, 1);
+        assert_eq!(world.live_neighbours_count(1, 1), 3);
+    }
+}
