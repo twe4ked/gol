654d7b3ade903e30a78dfa79c94e0ac894e21437
Odin Dutton <odindutton@gmail.com>
2019-05-13 14:49:57 +0800

    Add simulate() bench

--- src/main.rs
+++ src/main.rs
@@ -276,4 +276,13 @@ mod tests {
             World::new(100, 100);
         });
     }
+
+    #[bench]
+    fn bench_simulate(b: &mut Bencher) {
+        let mut world = World::new(100, 100);
+
+        b.iter(|| {
+            world.simulate();
+        });
+    }
 }
